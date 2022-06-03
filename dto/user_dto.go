fbe8cb15b49e988ac9ed78848d347cf2e95282a6
ZenithNUC <lizengzhi1998@foxmail.com>
2022-06-03 18:52:32 +0800

    add:增加了添加乘客接口

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -3,12 +3,14 @@ package dto
 import "ticket-backend/model"
 
 type UserDto struct {
+	Id    uint   `json:"id,omitempty"`
 	Name  string `json:"name,omitempty"`
 	Phone string `json:"phone,omitempty"`
 }
 
 func ToUserDto(user model.User) UserDto {
 	return UserDto{
+		Id:    user.ID,
 		Name:  user.Name,
 		Phone: user.Phone,
 	}
--- router/router.go
+++ router/router.go
@@ -11,9 +11,10 @@ func NewRouter() *gin.Engine {
 	r.GET("/ping", controller.Ping)
 	v1 := r.Group("/api/v1")
 	{
-		v1.POST("user/register", controller.UserRegister)                         // 用户注册接口
-		v1.POST("user/login", controller.UserLogin)                               // 用户登录接口
-		v1.GET("user/info", middlewave.UserAuthMiddleware(), controller.UserInfo) // 用户信息接口
+		v1.POST("user/register", controller.UserRegister)                                  // 用户注册接口
+		v1.POST("user/login", controller.UserLogin)                                        // 用户登录接口
+		v1.GET("user/info", middlewave.UserAuthMiddleware(), controller.UserInfo)          // 用户信息接口
+		v1.POST("passenger/add", middlewave.UserAuthMiddleware(), controller.PassengerAdd) // 添加乘客
 	}
 	return r
 }
